1fbef42d3891fbf134512cae34e806dfe0dcc2d2
Florian Friedrich <friedrich@procster.de>
2021-01-31 20:01:34 +0100

    Commit version 1.0.0

---
+++ src/CrookModSystem.cs
@@ -0,0 +1,38 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+
+namespace Crook
+{
+    public class CrookModSystem : ModSystem
+    {
+        ICoreAPI api;
+
+        public override void Start(ICoreAPI api)
+        {
+            this.api = api;
+
+            api.RegisterBlockBehaviorClass("CrookOnLeaves", typeof(CrookOnLeavesBehavior));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Event.ServerRunPhase(EnumServerRunPhase.GameReady, addCrookOnLeavesBehavior);
+        }
+
+        private void addCrookOnLeavesBehavior()
+        {
+            Block[] leaveBlocks = api.World.SearchBlocks(new AssetLocation("game", "leaves-*"));
+            foreach (Block leaveBlock in leaveBlocks)
+            {
+                leaveBlock.BlockBehaviors = leaveBlock.BlockBehaviors.Append(new CrookOnLeavesBehavior(leaveBlock));
+            }
+
+            Block[] leaveBranchyBlocks = api.World.SearchBlocks(new AssetLocation("game", "leavesbranchy-*"));
+            foreach (Block leaveBranchyBlock in leaveBranchyBlocks)
+            {
+                leaveBranchyBlock.BlockBehaviors = leaveBranchyBlock.BlockBehaviors.Append(new CrookOnLeavesBehavior(leaveBranchyBlock));
+            }
+        }
+    }
+}
---
+++ src/CrookOnLeavesBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace Crook
+{
+    public class CrookOnLeavesBehavior : BlockBehavior
+    {
+        public CrookOnLeavesBehavior(Block block) : base(block)
+        {
+        }
+
+        public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, ref EnumHandling handling)
+        {
+            if (byPlayer.InventoryManager.ActiveHotbarSlot == null ||
+                byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack == null ||
+                byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack.Item == null ||
+                !byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack.Item.Code.Equals(new AssetLocation("crook", "crook")))
+            {
+                return;
+            }
+
+            if (world.Rand.Next(4) != 0) 
+            {
+                return; 
+            }
+
+            var woodType = this.block.FirstCodePart(2);
+            var sapplingBlock = world.GetBlock(new AssetLocation("game", $"sapling-{woodType}-free"));
+
+            world.SpawnItemEntity(new ItemStack(sapplingBlock), new Vec3d(pos.X + 0.5, pos.Y + 0.5, pos.Z + 0.5), null);
+        }
+    }
+}
